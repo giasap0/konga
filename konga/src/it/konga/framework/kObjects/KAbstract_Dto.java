7d5fe1b126364067ec4e264fd5a5b7f2c579adca
Giampaolo Saporito <gia.sap0@gmail.com>
2015-01-05 09:22:27 +0100

    ristrutturato abstract DTO

--- konga/src/it/konga/framework/kObjects/KAbstract_Dto.java
+++ konga/src/it/konga/framework/kObjects/KAbstract_Dto.java
@@ -1,7 +1,5 @@
 package it.konga.framework.kObjects;
 
-import java.io.InvalidObjectException;
-import java.io.ObjectInputValidation;
 import java.io.Serializable;
 
 /**
@@ -10,23 +8,8 @@ import java.io.Serializable;
  * @author Giampaolo Saporito
  * @date 24-09-2014
  */
-public abstract class KAbstract_Dto implements Serializable, ObjectInputValidation
+public abstract class KAbstract_Dto implements Serializable
 {
 	private static final long serialVersionUID = 7163853062626L;
-	private Long id;
-
-	public Long getId(){
-		return id;
-	}
-	public void setId(Long id){
-		this.id = id;
-	}
-
-	@Override
-	public void validateObject() throws InvalidObjectException
-	{
-		//validare l'oggetto
-		if( getId() < 0) throw new InvalidObjectException("L'id deve essere positivo");	
-	}
 
 }
--- konga/src/it/konga/framework/kObjects/KAbstract_User.java
+++ konga/src/it/konga/framework/kObjects/KAbstract_User.java
@@ -1,7 +1,5 @@
 package it.konga.framework.kObjects;
 
-import java.io.InvalidObjectException;
-
 import it.konga.framework.interfaces.IAccessLevel;
 
 public class KAbstract_User extends KAbstract_Dto
@@ -9,14 +7,6 @@ public class KAbstract_User extends KAbstract_Dto
 	private static final long serialVersionUID = 4792214461438742785L;
 
 	protected IAccessLevel _accessLevel;
-	
-	@Override
-	public void validateObject() throws InvalidObjectException
-	{
-		if( getId() < 0) throw new InvalidObjectException("L'id deve essere positivo");	
-		if( !_accessLevel.isValid() ) throw new InvalidObjectException("Livello di accesso non valido");	
-	}
-
 
 	public static String getSessionID()
 	{

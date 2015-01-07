e5d5270d1e9c46cd344fd05a003b126b24484ea1
Giampaolo Saporito <gia.sap0@gmail.com>
2015-01-07 16:09:50 +0100

    accesso a user session id tramite interfaccia

--- web_app/src/it/kongaImplementation/flusso/KBaseAction.java
+++ web_app/src/it/kongaImplementation/flusso/KBaseAction.java
@@ -5,6 +5,7 @@ import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
 
 import it.konga.framework.flusso.KAbstractAction;
+import it.konga.framework.kObjects.KAbstract_User;
 import it.kongaImplementation.dto.KUser;
 
 @InterceptorRef(value="kongaStack")
@@ -16,5 +17,5 @@ public abstract class KBaseAction extends KAbstractAction
 {
 	private static final long serialVersionUID = -4364274118192007158L;
 
-	public KUser getUser()		{return (KUser) this.getSession().get(KUser.getSessionID());}
+	public KUser getUser()		{return (KUser) this.getSession().get(KAbstract_User.getSessionID());}
 }

622f9aed828f559deaa58524259c71f8ecfd9ced
Giampaolo Saporito <gia.sap0@gmail.com>
2015-01-07 15:45:24 +0100

    fix interceptor check utente in sessione

--- web_app/src/it/kongaImplementation/actions/esempi/ACT_Home.java
+++ web_app/src/it/kongaImplementation/actions/esempi/ACT_Home.java
@@ -1,7 +1,7 @@
 package it.kongaImplementation.actions.esempi;
 
-import it.konga.framework.flusso.KAbstractAction;
 import it.kongaImplementation.dto.KUser;
+import it.kongaImplementation.flusso.KBaseAction;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
@@ -10,15 +10,16 @@ import org.apache.struts2.convention.annotation.Results;
 @Results({
 	@Result(name="go_home", type="tiles", location="konga.tiles.home")
 })
-public class ACT_Home extends KAbstractAction
+public class ACT_Home extends KBaseAction
 {
 	private static final long serialVersionUID = -7251065121878532439L;
 
 	@Action(value="/home")
 	public String home() throws Exception
 	{
-		KUser utente = (KUser) getSession().get(KUser.getSessionID() );
-		String isUtente = utente == null ? "no utente" : utente.getAccessLevel().getDescription();
+		KUser utente = getUser();
+		//in realtà questo non avviene mai perchè c'è l'ionterceptor che si assicura che l'utente abbia eseguito il login
+		String isUtente = utente == null ? "no utente" : utente.getAccessLevel().getDescription();	
 		System.out.println( isUtente );
 		return "go_home";
 	}
--- web_app/src/it/kongaImplementation/interceptors/KInterceptorCheckAccess.java
+++ web_app/src/it/kongaImplementation/interceptors/KInterceptorCheckAccess.java
@@ -1,14 +1,16 @@
 package it.kongaImplementation.interceptors;
 
-import it.konga.framework.flusso.KAbstractAction;
 import it.konga.framework.kObjects.KAbstract_User;
 import it.kongaImplementation.flusso.IStandardResults;
 
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 
 /** Interceptor che si preoccupa di controllare se esiste l'utente in sessione */ 
-public class KInterceptor_CheckAutorization  implements Interceptor
+public class KInterceptorCheckAccess  implements Interceptor
 {
 	private static final long serialVersionUID = -4209884179941225475L;
 
@@ -18,18 +20,19 @@ public class KInterceptor_CheckAutorization  implements Interceptor
 
 	@Override
 	public void init() {
+		System.out.println("init check access interceptor");
 	}
 
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception
 	{
-		KAbstract_User user = (KAbstract_User) ((KAbstractAction)invocation).getSession().get(KAbstract_User.getSessionID());
-		String ret = IStandardResults.RESULT_FORBIDDEN;
-		if( user != null )
-		{
-			ret = invocation.invoke();
-		}
-		return ret;
+		System.out.println("validazione accesso");
+		Map<String, Object> session = ActionContext.getContext().getSession();
+		KAbstract_User user = (KAbstract_User) (session.get( KAbstract_User.getSessionID() ));
+
+		if (user == null)
+			return IStandardResults.RESULT_FORBIDDEN;
+		return invocation.invoke();
 	}
 
 }//EO KInterceptor_CheckAutorization
